9efd17169c9ff080ea9e7f410193a9f90351be6e
DaPakoREE <dapakoreeee@gmail.com>
2026-01-13 09:14:39 +0100

    Implement displayed database and add 'add product' and 'remove product' functionality to the webpage

--- project_files/StockAPI/wwwroot/js/admin_panel.js
+++ project_files/StockAPI/wwwroot/js/admin_panel.js
@@ -1,71 +1,66 @@
-const data = [
-    { id : 1, name : "Marlboro Red (20-pack)", categoryID : 1, price : 89.00, stock : 100},
-    { id : 2, name : "Camel Blue (20-pack)", categoryID : 1, price : 85.00, stock : 100 },
-    { id : 3, name : "L&M Filter (20-pack)", categoryID : 1, price : 79.00, stock : 100 },
-    { id : 4, name : "Skruf Original Portion", categoryID : 1, price : 62.00, stock : 100 },
-    { id : 5, name : "Göteborgs Rapé White Portion", categoryID : 1, price : 67.00, stock : 100 },
+const addProductButton = document.getElementById("add-product-btn")
+const addProductForm = document.getElementById("add-product-form")
 
-    { id : 6, name : "Marabou Mjölkchoklad 100 g", categoryID : 2, price : 25.00, stock : 100 },
-    { id : 7, name : "Daim dubbel", categoryID : 2, price : 15.00, stock : 100 },
-    { id : 8, name : "Kexchoklad", categoryID : 2, price : 12.00, stock : 100 },
-    { id : 9, name : "Malaco Gott & Blandat 160 g", categoryID : 2, price : 28.00, stock : 100 },
-
-    { id : 10, name : "Korv med bröd", categoryID : 3, price : 25.00, stock : 100 },
-    { id : 11, name : "Varm toast (ost & skinka)", categoryID : 3, price : 30.00, stock : 100 },
-    { id : 12, name : "Pirog (köttfärs)", categoryID : 3, price : 22.00, stock : 100 },
-    { id : 13, name : "Färdig sallad (kyckling)", categoryID : 3, price : 49.00, stock : 100 },
-    { id : 14, name : "Panini (mozzarella & pesto)", categoryID : 3, price : 45.00, stock : 100 },
-
-    { id : 15, name : "Aftonbladet (dagens)", categoryID : 4, price : 28.00, stock : 100 },
-    { id : 16, name : "Expressen (dagens)", categoryID : 4, price : 28.00, stock : 100 },
-    { id : 17, name : "Illustrerad Vetenskap", categoryID : 4, price : 79.00, stock : 100 },
-    { id : 18, name : "Kalle Anka & Co", categoryID : 4, price : 45.00, stock : 100 },
-    { id : 19, name : "Allt om Mat", categoryID : 4, price : 69.00, stock : 100 },
-]
+const allProducts = []
 
 document.addEventListener("DOMContentLoaded", () => {
     fetchData()
-    populateTable()
+    waitForData()
 })
 
-function deleteProductButton(productID, productName) {
+addProductForm.onsubmit = function() {
+    var formData = new FormData(document.getElementById("add-product-form"))
+    formData = Object.fromEntries(formData)
+    formData.stock = formData.stock == "" ? 0 : formData.stock
+    console.log(fetch("/api/v1/products", {method: "POST", body: JSON.stringify(formData), headers: {"Content-Type": "application/json",}}))
+    window.location.reload()
+}
+
+async function deleteProductButton(productID, productName) {
     let doDelete = confirm(`Är du säker på att du vill ta bort ${productName} från produktlistan?`)
     if (doDelete) {
-        fetch(`/api/v1/products/${productID}`, {method: "DELETE"})
+        await fetch(`/api/v1/products/${productID}`, {method: "DELETE"})
     }
+    window.location.reload()
 }
 
 async function fetchData() {
-    let data = await fetch("/api/v1/products")
-    let formattedData = await data.text()
-    console.log(formattedData)
+    await fetch("/api/v1/products")
+        .then((response) => response.text())
+        .then((result) => {
+        const PRODUCTS_OBJ = JSON.parse(result);
+        PRODUCTS_OBJ.forEach(product => allProducts.push(product));
+        })
+        .catch((error) => console.error(error));
+}
+
+function waitForData() {
+  if (allProducts.length === 0) {
+    setTimeout(waitForData, 10);
+  } else {
+    populateTable()
+  }
 }
 
 function populateTable() {
     const tableContent = document.getElementById("table-content")
 
-    const AMOUNT_OF_COLUMNS = 7
+    const AMOUNT_OF_COLUMNS = 6 // The total amount of columns on the table
 
-    if (data && data.length > 0) {
-        data.forEach(product => {
+    if (allProducts && allProducts.length > 0) {
+        allProducts.forEach(product => {
             let productRow = document.createElement('TR')
 
-            for (let j = 0; j < AMOUNT_OF_COLUMNS; j++) {
+            for (let i = 0; i < AMOUNT_OF_COLUMNS; i++) {
                 const productDataCell = document.createElement('TD');
                 let cellContent;
-                switch(j) {
+                switch(i) {
                     case 0: cellContent = product.id; break;
-                    case 1: cellContent = product.categoryID; break;
+                    case 1: cellContent = product.category; break;
                     case 2: cellContent = product.name; break;
                     case 3: cellContent = product.price.toLocaleString("sv-SE") + " kr"; break;
                     case 4: cellContent = product.stock; break;
                     case 5: 
-                        cellContent = document.createElement('button') 
-                        cellContent.classList.add('editBtn')
-                        cellContent.setAttribute('data-id', product.id)
-                        cellContent.textContent = "Redigera"
-                        break
-                    case 6: 
                         cellContent = document.createElement('form')
                         cellContent.action = `/api/v1/products/${product.id}`
                         cellContent.method = "DELETE"
@@ -76,7 +71,7 @@ function populateTable() {
                         cellContent.appendChild(delBtn)
                         break
                 }
-                
+
                 if (typeof(cellContent) == "string" || typeof(cellContent) == "number") {
                     productDataCell.appendChild(document.createTextNode(cellContent));
                 } else {
